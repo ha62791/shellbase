891bd9a3f076bd71284348636e7d0670cd9d7d79
ha62791 <ha62791@hotmail.com>
2017-01-15 22:19:58 +0800

    add show version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,5 @@
 
 ## [Unreleased]
 ### Added
+- Show version (@ha62791)
 - Open/Close HBase shell (@ha62791)
--- shellbase.py
+++ shellbase.py
@@ -18,6 +18,7 @@ limitations under the License.
 import os
 import fcntl
 import time
+import select
 from subprocess import Popen, PIPE
 
 
@@ -25,12 +26,55 @@ class HBaseShell:
 
     def __init__(self):
         self.__shell = None
+        self.__shell_resp_timeout_sec = 10.0
 
 
-    def __setNonBlocking(fd):
+    def __set_non_blocking(fd):
         flags = fcntl.fcntl(fd, fcntl.F_GETFL)
         flags = flags | os.O_NONBLOCK
         fcntl.fcntl(fd, fcntl.F_SETFL, flags)
+
+
+    def __run_cmd_wait_output(self, cmd, onNewLinesHandler):
+        if not self.isOpen():
+            raise RuntimeError("Shell is not open")
+
+        if type(cmd) is not bytes:
+            raise ValueError("'cmd' should be of type 'bytes'")
+
+        # append endline
+        if not cmd.endswith(os.linesep.encode()):
+            cmd += os.linesep.encode()
+
+        p = self.__shell
+        p.stdout.read() # clear stdout
+        p.stderr.read() # clear stderr
+
+        # input cmd
+        p.stdin.write(cmd)
+        p.stdin.flush()
+
+        # wait for lines
+        endOfOutput = False
+        while not endOfOutput:
+            if select.select([p.stdout,],[],[],self.__shell_resp_timeout_sec)[0]:
+                endOfOutput = onNewLinesHandler(p.stdout.readlines())
+            else:
+                for line in p.stderr:
+                    print(line.decode())
+
+                raise TimeoutError("No response from shell for " + str(int(self.__shell_resp_timeout_sec)) + " seconds." + os.linesep + "Please check HBase health status and reopen HBaseShell.")
+
+        # print stderr
+        for line in p.stderr:
+            print(line.decode())
+
+
+    def setShellRespTimeout(self, sec):
+        if type(sec) is not int or sec < 10:
+            raise ValueError("'sec' should be an integer of at least 10")
+        else:
+            self.__shell_resp_timeout_sec = sec*1.0
 
 
     def isOpen(self):
@@ -39,20 +83,59 @@ class HBaseShell:
 
     def open(self, pathToHBaseShell):
         if self.isOpen():
-            return
+            self.close()
 
         p = Popen([pathToHBaseShell,"shell"], stdin=PIPE, stdout=PIPE, stderr=PIPE, bufsize=1)
-        HBaseShell.__setNonBlocking(p.stdout)
-        HBaseShell.__setNonBlocking(p.stderr)
+        HBaseShell.__set_non_blocking(p.stdout)
+        HBaseShell.__set_non_blocking(p.stderr)
         self.__shell = p
+
+        startUpOutput = ""
+
+        def onNewLinesHandler(lines):
+            nonlocal startUpOutput
+
+            for line in lines:
+                startUpOutput += line.decode()
+
+            return startUpOutput.endswith(os.linesep*3)
+
+
+        self.__run_cmd_wait_output(b"", onNewLinesHandler)
+        print(startUpOutput.strip())
 
 
     def close(self):
         if self.isOpen():
-            self.__shell.stdin.write(b'exit\n')
+            self.__shell.stdin.write(b'exit' + os.linesep.encode())
             self.__shell.stdin.flush()
             time.sleep(1)
             self.__shell.stdout.close()
             self.__shell.stderr.close()
             self.__shell.kill()
             self.__shell = None
+
+
+    def version(self):
+        output = ""
+        versionStr = ""
+        nextLineIsVersionStr = False
+
+        def onNewLinesHandler(lines):
+            nonlocal output
+            nonlocal versionStr
+            nonlocal nextLineIsVersionStr
+
+            for line in lines:
+                output += line.decode()
+                if versionStr == "":
+                    if nextLineIsVersionStr:
+                        versionStr = line.decode().strip()
+                    else:
+                        nextLineIsVersionStr = True
+
+            return output.endswith(os.linesep*2)
+
+
+        self.__run_cmd_wait_output(b"version", onNewLinesHandler)
+        return versionStr
